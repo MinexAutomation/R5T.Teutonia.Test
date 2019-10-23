65c50ca1b3e59db71f9a63f7b8194d28467e215b
MinexAutomation <minex.automation@gmail.com>
2019-10-23 00:09:37 -0700

    Initial commit of stub for file-system cloning operator test-fixture.

---
+++ source/R5T.Teutonia.Test/Code/Test Fixtures/FileSystemCloningOperatorTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace R5T.Teutonia.Test
+{
+    public abstract class FileSystemCloningOperatorTestFixture
+    {
+        #region Test-Fixture
+
+        public IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public StringlyTypedPathOperatorCombineTestFixture(IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        #endregion
+        
+
+        /// <summary>
+        /// Tests cloning the example file-system state from one file-system to another.
+        /// </summary>
+        [TestMethod]
+        public void TestCloneOperation()
+        {
+
+        }
+    }
+}
